6f2fdaa5efb906e4b0f6155b57191cbf750f8821
Areum0921 <a90907@gmail.com>
2021-01-15 15:17:52 +0900

    with python

---
+++ page118.py
@@ -0,0 +1,10 @@
+n,m = map(int,input().split(" "))
+a,b,c=map(int,input().split(" "))
+
+check=[[0] * m for _ in range(n)]
+print(check)
+#for i in range(n):
+ #   for j in range(m):
+  #      map_data.append(int(input().split(" ")))
+
+#print(map_data)
